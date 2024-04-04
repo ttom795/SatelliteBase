94ebdab2162dd6436d29f14e74f3aa8bc4d9deae
Toby <ttom795@aucklanduni.ac.nz>
2024-04-04 17:15:18 +1300

    initial push

---
+++ Assets/SatelliteReader.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SGP4Methods;
+using System;
+using System.IO;
+
+public class SatelliteReader : MonoBehaviour
+{
+    public GameObject satellitePrefab;
+    private List<GameObject> satellitesList = new List<GameObject>();
+
+    void Start()
+    {
+        StartCoroutine(LoadSatellites());
+    }
+
+    IEnumerator LoadSatellites()
+    {
+        string tleData = File.ReadAllText(System.IO.Path.Combine(Application.streamingAssetsPath, "gp.txt"));
+
+        // Split the data by new lines to get individual sets
+        string[] tleSets = tleData.Split(new[] { "\n" }, StringSplitOptions.RemoveEmptyEntries);
+
+        Debug.Log(tleSets.Length);
+        // Process each TLE set
+        for (int i = 0; i < tleSets.Length; i+=3)
+        {
+            SGP4Lib calc = new SGP4Lib();
+
+            // Now, tleLines array contains two lines of TLE data for each set
+            // You can pass these two lines to the twoline2rv function
+            string line1 = tleSets[i+1];
+            string line2 = tleSets[i+2];
+
+            Debug.Log("Line 1:" + line1);
+            Debug.Log("Line 2:" + line2);
+
+            double startmfe, stopmfe, deltamin;
+            SGP4Lib.elsetrec satrec = new SGP4Lib.elsetrec();
+
+            // Call twoline2rv method to convert TLE data to internal representation
+            calc.twoline2rv(line1, line2, 'c', 'e', 'a', SGP4Lib.gravconsttype.wgs72, out startmfe, out stopmfe, out deltamin, out satrec);
+            
+            double[] position = new double[3];
+            double[] velocity = new double[3];
+            double tsince = 129400;
+
+            calc.sgp4(ref satrec, tsince, position, velocity);
+
+            string joinedString = string.Join(", ", position);
+            Debug.Log(joinedString);
+            Debug.Log("");
+
+            // Create a new Satellite object and add it to the list
+            GameObject newSatellite = Instantiate(satellitePrefab);
+
+            newSatellite.GetComponent<SatelliteScript>().Initialize(name, position, velocity);
+
+            satellitesList.Add(newSatellite);
+
+            if (i % 50 == 0)
+            {
+                yield return null;
+            }
+        }
+        yield return null; // Yield to update after every satellite is instantiated
+    }
+}
---
+++ Assets/SatelliteScript.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class SatelliteScript : MonoBehaviour
+{
+    private string satelliteName;
+    private double[] position;
+    private double[] velocity;
+
+    public void Initialize(string name, double[] pos, double[] vel)
+    {
+        satelliteName = name;
+        position = pos;
+        velocity = vel;
+        transform.position = new Vector3((float)pos[0], (float)pos[1], (float)pos[2])*0.01f;
+    }
+
+    // Example method to display satellite information
+    public void DisplaySatelliteInfo()
+    {
+        Debug.Log("Satellite Name: " + satelliteName);
+        Debug.Log("Position: (" + position[0] + ", " + position[1] + ", " + position[2] + ")");
+        Debug.Log("Velocity: (" + velocity[0] + ", " + velocity[1] + ", " + velocity[2] + ")");
+    }
+
+    private void Update()
+    {
+        Vector3 rotationPoint = Vector3.zero;
+        Vector3 direction = new Vector3((float)velocity[0], (float)velocity[1], (float)velocity[2]);
+        float angle = 0.1f;
+
+        Vector3 rotationAxis = Vector3.Cross(direction, transform.position - rotationPoint).normalized;
+
+        // Create a rotation quaternion
+        Quaternion rotation = Quaternion.AngleAxis(angle, rotationAxis);
+
+        // Rotate the object around the rotation point
+        transform.position = rotation * (transform.position - rotationPoint) + rotationPoint;
+
+    }
+}
